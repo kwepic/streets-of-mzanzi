37c3ea90bf197b80c4e69b401878c50e1b8d8560
kwepic <tommunroe57@gmail.com>
2026-09-15 10:36:29 +0200

    Add files via upload

---
+++ online-config.js
@@ -0,0 +1,2 @@
+// HTTPS origin of the separately hosted match server. Blank means same-origin.
+export const serverUrl = '';
---
+++ stages.js
@@ -0,0 +1,10 @@
+export const STAGES=[
+ {name:'Table Mountain Waterfront',atlas:'cape',row:0,rows:5,ground:.89,water:true},
+ {name:'Bo-Kaap Streets',atlas:'cape',row:1,rows:5,ground:.84},
+ {name:'Sea Point Promenade',atlas:'cape',row:2,rows:5,ground:.82,water:true},
+ {name:'Grand Parade',atlas:'cape',row:3,rows:5,ground:.83},
+ {name:'Muizenberg Beach',atlas:'cape',row:4,rows:5,ground:.82,water:true},
+ {name:'Long Street After Dark',atlas:'newStages',row:0,rows:3,ground:.76},
+ {name:'Durban Golden Mile',atlas:'newStages',row:1,rows:3,ground:.76,water:true},
+ {name:'Joburg Skyline Rooftop',atlas:'newStages',row:2,rows:3,ground:.76}
+];
